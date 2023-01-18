1525dbe7b19bc19b30b6e669825e54fe195dd5d5
yesimcetintas <yesimturker@outlook.com>
2023-01-18 15:03:58 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int time = DateTime.Now.Hour;
+if(time>6 && time<11)
+    Console.WriteLine("Günaydın");
+else if( time<=18)
+    Console.WriteLine("İyi Günler.");
+else
+    Console.WriteLine("İyi Geceler");
+
+string sonuc = time<18 ? "İyi Günler" : "İyi Geceler";
+
+sonuc = time>6 && time<11 ? "Günaydın" : time<18 ? "İyi Günler" : "İyi Geceler";
+Console.WriteLine(sonuc);
